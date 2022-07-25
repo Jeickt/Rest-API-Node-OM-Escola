f15e45b566516591efb9799076418c2543ffc426
Jeison Leandro Rückert <jeison.let@gmail.com>
2022-07-24 21:09:58 -0300

    Configuração de CORS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import dotenv from 'dotenv';
 import './database/connection';
 import { resolve } from 'path';
@@ -8,10 +10,21 @@ import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import studentRoutes from './routes/studentRoutes';
 
+const securedUrlList = [process.env.APP_URL_FRONTEND, process.env.APP_URL_FRONTEND_DEV];
+
+const corsOptions = {
+  origin: (origin, cb) => {
+    if (!origin || securedUrlList.indexOf(origin) !== -1) cb(null, true);
+    else cb(new Error('Não permitido por CORS'));
+  },
+};
+
 dotenv.config();
 
 const app = express();
 
+app.use(cors(corsOptions));
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(resolve(__dirname, 'uploads')));
