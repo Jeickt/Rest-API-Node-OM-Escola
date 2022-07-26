e0c4c97a655509701f6fc9139b68d33fa6680fdb
Jeison Leandro Rückert <jeison.let@gmail.com>
2022-07-25 22:46:07 -0300

    try to fix Heroku h18 error

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -15,9 +15,8 @@ exports.create = (req, res) => upload(req, res, async (err) => {
     const { studentId } = req.body;
     const photo = await Photo.create({ originalName: originalname, fileName: filename, studentId });
 
-    return res.status(200).json(photo);
+    return setTimeout(() => res.status(200).json(photo), 2000);
   } catch (e) {
-    return res.status(400).json(e);
-    // { errors: ['O estudante não existe'] }
+    return res.status(400).json({ errors: ['O estudante não existe'] });
   }
 });
