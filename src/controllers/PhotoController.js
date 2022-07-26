26581fb90de7f9502a3425cbf1df96dcb79a9bd5
Jeison Leandro Rückert <jeison.let@gmail.com>
2022-07-26 15:27:33 -0300

    Verificação de ocorrência de erro

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -10,13 +10,13 @@ exports.create = (req, res) => upload(req, res, async (err) => {
     return res.status(400).json({ errors: [err.code] });
   }
 
-  try {
-    const { originalname, filename } = req.file;
-    const { studentId } = req.body;
-    const photo = await Photo.create({ originalName: originalname, fileName: filename, studentId });
+  // try {
+  const { originalname, filename } = req.file;
+  const { studentId } = req.body;
+  const photo = await Photo.create({ originalName: originalname, fileName: filename, studentId });
 
-    return setTimeout(() => res.status(200).json(photo), 2000);
-  } catch (e) {
-    return res.status(400).json({ errors: ['O estudante não existe'] });
-  }
+  return setTimeout(() => res.status(200).json(photo), 2000);
+  // } catch (e) {
+  //   return res.status(400).json({ errors: ['O estudante não existe'] });
+  // }
 });
